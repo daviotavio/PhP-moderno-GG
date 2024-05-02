8aff56a494efdf2b196d7b2b458f830ede084a27
daviotavio <daviotavioribeiro@gmail.com>
2024-05-02 19:16:09 -0300

    Primeiro Commit

---
+++ desafios/D001/resposta.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Desafio PhP</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <main>
+    <h1>Resultado final</h1>
+    <p>
+      <?php
+      $n = $_REQUEST["num"];
+      $a = $n - 1;
+      $s = $n + 1;
+      echo "O número escolhido foi <strong>$n</strong>";
+      echo "<br>O seu <em>antecessor</em> é <strong>$a</strong>";
+      echo "<br>O seu <em>sucessor</em> é <strong>$s</strong>";
+      ?>
+    </p>
+    <button onclick="javascript:history.go(-1)">&#x2b05 Voltar</button>
+  </main>
+</body>
+
+</html>
