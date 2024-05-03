f2b3f9c861e673579e576bed8dd8816e2258649a
daviotavio <daviotavioribeiro@gmail.com>
2024-05-02 21:18:25 -0300

    melhorias no desafio 2

--- desafios/D002/index.php
+++ desafios/D002/index.php
@@ -12,11 +12,15 @@
   <main>
     <h1>Trabalhando com números aleatórios</h1>
     <?php
-    $n = mt_rand(1, 100);
-    echo "<p>Gerando um número aleatório entre 1 e 100</p>";
+    $num1 = $_REQUEST["num1"];
+    $num2 = $_REQUEST["num2"];
+
+    $n = mt_rand($num1, $num2);
+    echo "<p>Gerando um número aleatório entre $num1 e $num2</p>";
     echo "O número gerado foi $n";
     ?>
     <button onclick="javascript:document.location.reload()">&#X1F504; Gerar novo</button>
+    <button onclick="javascript:history.go(-1)">&#x2b05 Voltar</button>
   </main>
 </body>
 
