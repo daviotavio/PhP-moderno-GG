3ceee1e47c2c0a3a53aded366b979b906aa3bef1
Davi Ribeiro <davi@conasems.local>
2024-05-08 14:06:36 -0300

    Adicionado pastas e arquivos do PC da empresa pro repositorio online

---
+++ exercicios/ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="widt'h=device-width, initial-scale=1.0">
+  <title>Me livrando da Maldição</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "Olá, Mundo! \u{1F30E}";
+    ?>
+  </h1>
+  <p>Vamos tentar nos livrar da maldição</p>
+</body>
+
+</html>
---
+++ exercicios/ex003/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Teste de tipos primitivos</h1>
+  <?php
+  // $num = 0x1A;
+  // echo "o valor da variável é $num";
+
+  // $v = 300;
+  // var_dump($v);
+
+  $num = 3e2;
+  echo "o valor da variável é $num"
+  ?>
+</body>
+
+</html>
