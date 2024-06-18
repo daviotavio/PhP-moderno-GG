0aefbb7e9ed0ba244d7beaee7b7966dcf145c2e8
Davi Ribeiro <davi@conasems.local>
2024-06-18 09:49:26 -0300

    incluido novo arquivo

--- POO/index.php
+++ POO/index.php
@@ -1,40 +1,6 @@
 <?php
 
-class Ninja
-{
-  public $traje;
-  public $arma;
-
-  public function setTraje($traje)
-  {
-    $this->traje = $traje;
-  }
-
-  public function getTraje()
-  {
-    return $this->traje;
-  }
-
-  public function setArma($arma)
-  {
-    $this->arma = $arma;
-  }
-
-  public function getArma()
-  {
-    return $this->arma;
-  }
-
-  public function atacar()
-  {
-    echo "o ninja está atacando" . "<br>";
-  }
-
-  public function defender()
-  {
-    echo "o ninja está defendendo" . "<br>";
-  }
-}
+include __DIR__ . "/ninja.php";
 
 
 $ninja = new Ninja();
